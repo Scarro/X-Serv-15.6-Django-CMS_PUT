0627d2621af6fc45c68648b0c7d7f4bc4fee1215
scarro <sergioca22@gmail.com>
2016-05-14 13:07:08 +0200

    actualizando

---
+++ cms/models.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Pages(models.Model):
+    nombre = models.CharField(max_length=200)
+    recurso = models.TextField()
+
+    def __str__(self):
+        return self.nombre
